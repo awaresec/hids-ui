863cb007ffe50716be19219b97cf108d978b3478
Gael Muller <gael@awaresec.org>
2011-12-22 19:16:44 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HidsUi::Application.config.secret_token = '509682ac2cb9002ded94db1e004b53f1d1fc4a2cf99942df3b6acbb5ca73dde40c72a07f1f7e153e83e992a73668ad45b9153c4954cadef07cafd1bca2350330'
