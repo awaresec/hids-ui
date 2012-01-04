9bebdb6a7708bd0c4951ea222618c0555f86267e
Gael Muller <gael@awaresec.org>
2012-01-04 16:58:51 +0100

    Changed display of level to text (Critical, Major, Minor, Info).

--- app/views/alerts/index.rb
+++ app/views/alerts/index.rb
@@ -3,5 +3,21 @@ module Alerts
     def alert_details_template
       template_include_tag "alerts/alert"
     end
+    
+    def level_text
+      if context[:level] > 10
+        "Critical"
+      elsif context[:level] > 7
+        "Major"
+      elsif context[:level] > 3
+        "Minor"
+      else
+        "Info"
+      end
+    end
+    
+    def time_text
+      context[:time].to_s(:db)
+    end
   end
 end
